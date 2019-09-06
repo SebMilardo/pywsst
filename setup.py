37f2219eafea353cf59b90ba7df29a0758148532
Sebastiano Milardo <s.milardo@hotmail.it>
2019-09-06 01:05:57 -0400

    Add setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+import setuptools
+
+classifiers = ['Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: 2-Clause BSD License',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Mathematics',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Programming Language :: Python :: 3']
+
+
+setup(name='pywsst',
+    version='0.0.1',
+    description='Wavelet synchrosqueezed transform in Python',
+    author='Sebastiano Milardo',
+    author_email='milardo@mit.edu',
+    license = '2-Clause BSD License',
+    url = 'https://github.com/SebMilardo/pywsst'
+    )
